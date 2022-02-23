a7c06cfa634c504b3c5e39e9c8d302887d5997d3
Konrino <konrino@gmail.com>
2022-02-23 23:02:17 +0100

    update3

--- app/tests/ViewsTests.py
+++ app/tests/ViewsTests.py
@@ -10,7 +10,7 @@ class IntegrationTest(unittest.TestCase):
         response = requests.get(
             host + '/spolka-filter/analiza?spolka_nazwa=ALLEGRO&inwestycja=5000&data_od=2021-10-01&data_do=2021-10-22')
         json_data = response.json()
-        self.assertEqual(json_data['spolka'], 'ALlEGRO')
+        self.assertEqual(json_data['spolka'], 'ALLEGRO')
         self.assertEqual(json_data['roi'], '-13.339%')
         self.assertEqual(json_data['kurs_max'], 59.88)
         self.assertEqual(json_data['data_dodania_max'], '10/11/2021')
